c5aef4851bb6d4fec5e6b528232c16ff0ac2e46b
Sho Maekawa <s.m@Shos-MacBook-Pro.local>
2024-09-20 11:50:46 +0900

    create a MyTrip-app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('navbar-brand')
+    My Trip
+@endsection
+
+@section('content')
+    <h1>My Trip</h1>
+    <div class="container">
+
+    </div>
+
+@endsection
+
